1b16bd14ea233ad17c58edd00fc693d57f0dba95
DiegoCebrian <98849460+DiegoCebrian@users.noreply.github.com>
2022-02-19 11:15:02 +0100

    Delete Tarea2 directory

--- Tarea2/Ejercicio5.py
+++
@@ -1,9 +0,0 @@
-"""
-  Se creará una base de datos MySQL (situada en un servidor de bases de datos local o en Internet) con la tabla repositorios (id, name, html_url, description y created_at).
-
-    Crear un programa en lenguaje Python que pida un nombre de usuario en GitHub, utilice el api de GitHub para obtener los datos de de los repositorios de ese usuario y los almacene en la tabla repositorios (los id, name, html_url, description y created_at). También se mostrarán en pantalla los nombres de los repositorios y el número total de repositorios que tiene ese usuario.
-
-    Se tratarán todos los errores que se pueden dar en el proceso.
-
-    También se entregarán  las instrucciones de creación de la base de datos.
-"""
